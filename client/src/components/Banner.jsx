10a41cabd7f36105b1bc39defe110b38773bdf0c
KatrinCl <rybina.ekaterina.93@mail.ru>
2026-05-10 15:25:46 +0300

    PawCare

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
     <div className='flex flex-col md:flex-row gap-4 m-2 md:m-10'>
       <div className='flex flex-1 flex-col gap-10 border border-gray-100 shadow-xl rounded-2xl p-8 md:p-18'>
         <h1 className='text-lg md:text-4xl'>
-          Ветеринарная клиника <span className='font-bold'>VetClinic</span> в Видном
+          Ветеринарная клиника <span className='font-bold'>PawCare</span> в Видном
         </h1>
         <p className='text-gray-700'>Время работы: Пн-вс 10.00-22.00</p>
       </div>
